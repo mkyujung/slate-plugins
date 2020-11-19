309d93d094a167430395e14d474bb96511d45eee
mkyujung <mkyujung@gmail.com>
2020-11-20 10:34:30 +1300

    remove dist from git.ignore

---
+++ packages/core/dist/utils/index.d.ts
@@ -0,0 +1,6 @@
+export * from './decoratePlugins';
+export * from './onDOMBeforeInputPlugins';
+export * from './onKeyDownPlugins';
+export * from './renderElementPlugins';
+export * from './renderLeafPlugins';
+//# sourceMappingURL=index.d.ts.map
